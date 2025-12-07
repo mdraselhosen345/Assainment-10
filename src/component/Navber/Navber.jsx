c6982f396288bd84fd3046706cf6e12f70595aa3
md rasel islam <rashel099009@gamil.com>
2025-12-07 08:36:55 +0600

    added in file

--- src/component/Navber/Navber.jsx
+++ src/component/Navber/Navber.jsx
@@ -17,7 +17,7 @@ const Navber = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">RASEL</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
